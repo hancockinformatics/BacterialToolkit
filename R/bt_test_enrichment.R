e4553843a6a289b69fd320ad111988270ab94aca
Travis <travis.m.blimkie@gmail.com>
2019-09-04 20:02:40 -0700

    Adding Functions and Docs
    Added the first three functions, and associated documentation.

---
+++ R/bt_test_enrichment.R
@@ -0,0 +1,37 @@
+#' bt_test_enrichment
+#'
+#' @param query_genes Genes to be tested for enrichment
+#' @param enrichment_set Genes associated with a function/pathway
+#' @param total_genes Total number of genes
+#'
+#' @return
+#' @export
+#'
+#' @description
+#' This function is designed to perform enrichment on a list of genes of
+#' interest, comparing to some specified background. Uses Fisher's Exact Test for
+#' p-value.
+bt_test_enrichment <- function(query_genes, enrichment_set, total_genes) {
+
+  require(tidyverse)
+
+
+  # Calculate overlap between the query list and enrichment set
+  num_overlap <- as.numeric(length(intersect(query_genes, enrichment_set)))
+
+
+  # Construct the matrix to be used for the test
+  enrichment_matrix <- matrix(c(
+    num_overlap,
+    as.numeric(length(enrichment_set) - num_overlap),
+    as.numeric(length(query_genes) - num_overlap),
+    as.numeric(total_genes - (length(enrichment_set) - num_overlap))
+  ), nrow = 2, ncol = 2)
+
+
+  # Run and return Fisher's Exact test on the matrix
+  raw_pval <- fisher.test(enrichment_matrix, alternative = "greater")$p.value
+
+  return(raw_pval)
+
+}
---
+++ R/bt_tidy_gage.R
@@ -0,0 +1,26 @@
+#' bt_tidy_gage
+#'
+#' @param gage_result Results object from Gage enrichment
+#' @param qval Desired cutoff for significance
+#'
+#' @return
+#' @export
+#'
+#' @description
+#' Takes a results object from Gage and munges it into a tidy data frame Also
+#' filters based on q-value, with a default of 0.1 from the Gage documentation.
+#'
+#' @author Travis Blimkie
+#'
+bt_tidy_gage <- function(gage_result, qval = 0.1) {
+
+  require(tidyverse)
+
+  bind_rows(
+    list(Up = as.data.frame(gage_result[["greater"]]) %>% rownames_to_column(),
+         Down = as.data.frame(gage_result[["less"]]) %>% rownames_to_column()),
+    .id = "Direction"
+  ) %>%
+    filter(., q.val < qval)
+
+}
